74db084b0481877cffe6e06c1ca4ffe7bbcbea08
Kelvin Kyendwa <kelvinkyendwa@gmail.com>
2022-09-26 15:47:41 +0300

    set up basic firebase and routes  - set up signup route  - set up sign-in route  - set up JWT verification link

---
+++ auth/main.py
@@ -0,0 +1,78 @@
+from typing import Union
+import firebase_admin
+import pyrebase
+import json
+import uvicorn
+
+from firebase_admin import credentials, auth
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+
+cred = credentials.Certificate('mawazo-firebase.json')
+firebase = firebase_admin.initialize_app(cred)
+
+pb = pyrebase.initialize_app(json.load(open('firebase_config.json')))
+app = FastAPI()
+allow_all = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_all,
+    allow_credentials=True,
+    allow_methods=allow_all,
+    allow_headers=allow_all
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# signup endpoint
+@app.post("/signup", include_in_schema=True)
+async def signup(request: Request):
+    req = await request.json()
+    # req = request
+    email = req['email']
+    password = req['password']
+    if email is None or password is None:
+        return HTTPException(detail={'message': 'Error! Missing Email or Password'}, status_code=400)
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+        return JSONResponse(content={'message': f'Successfully created user {user.uid}'}, status_code=200)
+    except:
+        return HTTPException(detail={'message': 'Error Creating User'}, status_code=400)
+
+
+# login endpoint
+@app.post("/login", include_in_schema=False)
+async def login(request: Request):
+    req_json = await request.json()
+    email = req_json['email']
+    password = req_json['password']
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return JSONResponse(content={'token': jwt}, status_code=200)
+    except:
+        return HTTPException(detail={'message': 'There was an error logging in'}, status_code=400)
+
+
+# verification endpoint
+@app.post("/verify", include_in_schema=False)
+async def validate(request: Request):
+    headers = request.headers
+    jwt = headers.get('authorization')
+    print(f"jwt:{jwt}")
+    user = auth.verify_id_token(jwt)
+    return user["uid"]
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
